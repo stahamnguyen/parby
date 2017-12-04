d3ba238b4436cdf261b66117d47c370b1bb47c2a
Toan Thanh <thanhductoan1997@Toan-Thanhs-iMac.local>
2017-12-04 05:47:39 +0200

    test

---
+++ resources/js/home.js
@@ -0,0 +1,67 @@
+/*  ***************************************************
+                Display modal post form
+***************************************************  */
+
+
+const modal = document.getElementById("modal-container");
+const inputCaption = document.getElementById("input-caption");
+
+const inputImg = document.getElementById("uploadImg");
+
+const closeBtn = document.getElementsByClassName("close")[0];
+
+let image = document.getElementById("modal-input").getElementsByTagName("img")[0];
+
+inputImg.addEventListener("change", (event) => {
+    
+    let file = event.target.files[0];
+
+    let reader = new FileReader();
+
+    // Closure to capture the file information.
+    reader.onload = (event) => {
+        image.src = event.target.result;
+    };
+
+    // Read in the image file as a data URL.
+    reader.readAsDataURL(file);
+
+    modal.style.display = "block";
+});
+
+function closeModal() {
+    modal.style.display = "none";
+    inputCaption.value = "";
+    inputImg.value = "";
+    image.src = "";
+}
+
+closeBtn.onclick = function() {
+    closeModal();
+}
+window.onclick = function(event) {
+    if (event.target === modal) {
+        closeModal();
+    }
+}
+
+
+/*  ***************************************************
+                Display comment
+***************************************************  */
+
+
+const commentBtnArray = [...document.getElementsByClassName("comment-btn")];
+
+commentBtnArray.forEach(element => {
+    element.addEventListener("click", () => {
+        let post = element.parentNode.parentNode;
+        let commentSection = post.getElementsByClassName("comment-section")[0];
+        let commentInputSection = post.getElementsByClassName("comment-input-section")[0];
+        
+        commentSection.classList.toggle("hidden");
+        commentInputSection.classList.toggle("hidden");
+    });
+});
+
+
