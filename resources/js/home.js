55630b6b7466a63c2dddb952580fef4ef5650b57
Toan Thanh <thanhductoan1997@gmail.com>
2017-12-06 02:15:54 +0200

    add screen changes when upload new file + add option button to photoPage

--- resources/js/home.js
+++ resources/js/home.js
@@ -13,7 +13,7 @@ const closeBtn = document.getElementsByClassName("close")[0];
 let image = document.getElementById("modal-input").getElementsByTagName("img")[0];
 
 inputImg.addEventListener("change", (event) => {
-    
+
     let file = event.target.files[0];
 
     let reader = new FileReader();
@@ -56,7 +56,7 @@ const container = document.getElementById("container");
 
 postBtn.addEventListener("click", () => {
 
-    const postHtml = 
+    const postHtml =
                     '<div class="post">\
                         <div class="post-header">\
                             <div>\
@@ -112,13 +112,13 @@ postBtn.addEventListener("click", () => {
 
 let mapCommentButton = () => {
     let commentBtnArray = [...document.getElementsByClassName("comment-btn")];
-    
+
     commentBtnArray.forEach(element => {
         element.addEventListener("click", () => {
             let post = element.parentNode.parentNode;
             let commentSection = post.getElementsByClassName("comment-section")[0];
             let commentInputSection = post.getElementsByClassName("comment-input-section")[0];
-            
+
             commentSection.classList.toggle("hidden");
             commentInputSection.classList.toggle("hidden");
         });
@@ -138,14 +138,14 @@ let mapPostOptionButton = () => {
     let optionButtonArray = [...document.getElementsByClassName("post-option-btn")];
     let optionButtonIconArray = [...document.getElementsByClassName("post-option-btn-icon")];
     let dropdownArray = [...document.getElementsByClassName("dropdown")];
-    
+
     optionButtonArray.forEach(element => {
         let dropdown = dropdownArray[optionButtonArray.indexOf(element)];
-    
+
         element.addEventListener("click", () => {
             dropdown.classList.toggle("hidden");
         });
-    
+
         dropdown.addEventListener("focusout", () => {
             dropdown.classList.add("hidden");
         });
@@ -163,10 +163,10 @@ mapPostOptionButton();
 let mapDeletePostButton = () => {
     let postsArray = [...document.getElementsByClassName("post")];
     let deleteButtonArray = [...document.getElementsByClassName("post-delete-btn")];
-    
+
     deleteButtonArray.forEach(element => {
         let post = postsArray[deleteButtonArray.indexOf(element)];
-    
+
         element.addEventListener("click", () => {
             post.parentNode.removeChild(post);
         });
--- resources/js/photo.js
+++ resources/js/photo.js
@@ -4,20 +4,44 @@
 
 
 const modal = document.getElementById("modal-container");
-const addAlbumModal = document.getElementById("modal-addAlbum");
 const inputCaption = document.getElementById("input-caption");
 
+const inputImgButton = document.getElementById("addImage");
 const inputImg = document.getElementById("uploadImg");
-
 
 const closeBtn = document.getElementsByClassName("close")[0];
 
 let image = document.getElementById("modal-input").getElementsByTagName("img")[0];
 
-
+/*  ***************************************************
+                Display modal from nav button
+***************************************************  */
 inputImg.addEventListener("change", (event) => {
 
     console.log("hi");
+
+    var file = event.target.files[0];
+
+    var reader = new FileReader();
+
+    // Closure to capture the file information.
+    reader.onload = (event) => {
+        image.src = event.target.result;
+    };
+
+    // Read in the image file as a data URL.
+    reader.readAsDataURL(file);
+
+    modal.style.display = "block";
+});
+
+/*  ***************************************************
+                Display modal from button
+***************************************************  */
+
+inputImgButton.addEventListener("change", (event) => {
+
+    console.log("moikka");
 
     var file = event.target.files[0];
 
@@ -53,14 +77,37 @@ window.onclick = function(event) {
     }
 }
 
+/*  ***************************************************
+                    New Photo Added To Screen
+***************************************************  */
 
-// Get the modal
-var imgModal = document.getElementById('imgModal');
+
+const postBtn = document.getElementById("post-btn");
+const showcase = document.getElementById("showcase");
+
+postBtn.addEventListener("click", () => {
+
+    const postHtml = '<li>\
+                        <figure class="album-photo">\
+                          <img src="'+ image.src + '" id="img1">\
+                        </figure>\
+                      </li>'
+
+
+    showcase.innerHTML = postHtml + showcase.innerHTML;
+    closeModal();
+});
+
+
+/*  ***************************************************
+                Display Picture modal
+***************************************************  */
+const imgModal = document.getElementById('imgModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('img1');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+const img = document.getElementById('img1');
+const modalImg = document.getElementById("img01");
+const captionText = document.getElementById("caption");
 img.onclick = function(){
     imgModal.style.display = "block";
     modalImg.src = this.src;
@@ -68,7 +115,7 @@ img.onclick = function(){
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[1];
+const span = document.getElementsByClassName("close")[1];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
